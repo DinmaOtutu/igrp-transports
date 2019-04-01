cd76b01e5f8f951a15314a1e5968f3cb95a9bb7f
geraldanosike <geraldanosike.javhse@gmail.com>
2019-04-01 11:56:49 +0100

    validation edit

--- server/middlewares/agentvalidation.js
+++ server/middlewares/agentvalidation.js
@@ -5,31 +5,21 @@ function validateAgentInput(user) {
   const schema = {
     errors: Joi.object(),
     isLoading: Joi.boolean(),
-    phoneNumber: Joi.string()
-      .required()
-      .min(10),
+    phoneNumber: Joi.string().min(10),
     password: Joi.string()
       .min(6)
-      .max(30)
-      .required(),
+      .max(30),
     fullname: Joi.string()
       .min(6)
-      .max(30)
-      .required(),
-    address: Joi.string()
-      .min(6)
-      .required(),
-    email: Joi.string().required(),
-    age: Joi.number().required(),
-    guarantorsFullName: Joi.string().required(),
-    guarantorsPhonenumber: Joi.number()
-      .required()
-      .min(10),
-    guarantorsAddress: Joi.string().required(),
-    meansOfId: Joi.string().required(),
-    idNumber: Joi.string()
-      .required()
-      .min(4)
+      .max(30),
+    address: Joi.string().min(6),
+    email: Joi.string(),
+    age: Joi.number(),
+    guarantorsFullName: Joi.string(),
+    guarantorsPhonenumber: Joi.number().min(10),
+    guarantorsAddress: Joi.string(),
+    meansOfId: Joi.string(),
+    idNumber: Joi.string().min(4)
   };
 
   return Joi.validate(user, schema);
