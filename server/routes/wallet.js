1d33198297665b70727f7717e4161b3dc1626220
DinmaOtutu <otutudinma1995@gmail.com>
2019-04-04 13:26:40 +0100

    wallet issues

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -3,12 +3,14 @@ const auth = require('../middlewares/authentication');
 const SettingController = require('../controllers/SettingController');
 const  WalletController = require('../controllers/WalletController')
 const InstapayController = require('../controllers/InstapayController');
+const TransactionHistoryControoller = require('../controllers/TransactionHistoryController');
 
 const router = Router();
 
 const { activatedWallet, walletCodeReset, walletCodeRecovery, checkBalance, sendMoney } = WalletController;
 const { addBankData, removeBankData } = SettingController;
-const { getBankList, creditWallet, bankTransfer, webHook } = InstapayController
+const { getBankList, creditWallet, bankTransfer, webHook } = InstapayController;
+const { getTransactionHistory } = TransactionHistoryControoller;
 
 router.use(auth);
 router.put('/addbank', addBankData);
@@ -21,6 +23,7 @@ router.post('/sendmoney', sendMoney);
 router.post('/fundwallet', creditWallet);
 router.post('/debitwallet', bankTransfer);
 router.get('/banklist', getBankList);
+router.get('/transactionHistory', getTransactionHistory);
 
 
 module.exports = router;
